dda2cf66d8f84126c9702cfba8195d6bede042ad
Emily Jirles <emily.jirles@gmail.com>
2018-02-08 19:11:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,4 +22,12 @@ function currentLine(line){
   // returns current line
   //"The line is currently: 1. Ada, 2. Grace"
   // if no one "The line is currently empty"
+  if (line.length === 0){
+    return "The line is currently empty."
+  } else {
+    var message = `The line is currently: 1. ${line[0]},`
+    for (var i = 0; i < line.length; ++){
+      
+    }
+  }
 }
