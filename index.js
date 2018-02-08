7076b47905ecf0c0bc0cd7cb4e48a7b82c0c601a
Emily Jirles <emily.jirles@gmail.com>
2018-02-08 18:57:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function takeANumber(line, name){
+  //accepts current line of people, var katzDeliLine
+  //also accepts name of person taking a Number
+  //must return index + 1 place in line to customer
+  //ex. 'Welcome, Ada. You are number 1 in line.'
+}
+
+function 
