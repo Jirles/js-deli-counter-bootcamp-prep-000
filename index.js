5f16651db3616e2219a99a9ba26dd69f2fa15332
Emily Jirles <emily.jirles@gmail.com>
2018-02-08 19:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ function takeANumber(line, name){
   //also accepts name of person taking a Number
   //must return index + 1 place in line to customer
   //ex. 'Welcome, Ada. You are number 1 in line.'
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
 
 function nowServing(line){
   //return first person in line and remove that individual from line
-  // if nobody in line return "There is nobody waiting to be served"
+  // if nobody in line return "There is nobody waiting to be served!"
+  // 'Currently serving Ada.''
+  if line.length === 0{
+    return "There is nobody waiting to be served!"
+  } else{
+    return `Currently serving ${line.shift()}.`
+  }
 }
 
 function currentLine(line){
